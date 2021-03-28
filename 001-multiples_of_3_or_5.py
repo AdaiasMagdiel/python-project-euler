0c761a8d1198df19333b413455af4f0df73f52b1
AdaiasMagdiel <adaiasmagdiell@gmail.com>
2021-03-28 19:55:04 -0300

    refact: new way to resolve the question.

--- 001-multiples_of_3_or_5.py
+++ 001-multiples_of_3_or_5.py
@@ -1,7 +1,10 @@
-multiples = []
+def old_way():
+    multiples = []
+    for number in range(1000):
+        if number % 3 == 0 or number % 5 == 0:
+            multiples.append(number)
 
-for number in range(1000):
-    if number % 3 == 0 or number % 5 == 0:
-        multiples.append(number)
+    print(sum(multiples))
 
-print(sum(multiples))
+
+print(sum([i for i in range(1000) if i % 5 == 0 or i % 3 == 0]))
